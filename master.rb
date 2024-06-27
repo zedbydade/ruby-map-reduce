5071ef6948842f4da6c052012a72d2ff3ed8ba5c
Francisco Paradela <franciscoleite.dev@protonmail.com>
2024-06-26 23:21:24 -0300

    Add split_files

--- master.rb
+++ master.rb
@@ -1,4 +1,5 @@
 require 'grpc'
+require 'digest'
 require_relative './lib/server_services_pb'
 
 class Master < MapReduceMaster::Service
@@ -9,8 +10,9 @@ class Master < MapReduceMaster::Service
     attr_accessor :data
   end
 
-  def initialize(worker_timeout: 10)
+  def initialize(worker_timeout: 10, worker_count: 2)
     @worker_timeout = worker_timeout
+    @worker_count = worker_count
     @data = {}
   end
 
@@ -27,14 +29,21 @@ class Master < MapReduceMaster::Service
   ensure
     self.class.data.delete(maps_req.id)
   end
-end
 
-def master
-  server = GRPC::RpcServer.new
-  server.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
-  server.handle(Master)
-  p 'Running server'
-  server.run_till_terminated
-end
+  def split_files(key, file)
+    encrypt_key = generate_digest_key(key)
+    FileUtils.mkdir_p("./files/#{encrypt_key}")
+    line_maximum = (File.open(file).count / @worker_count).to_i
+    file_data = file.readlines.map(&:chomp)
+    file_number = file_data.length / line_maximum
+    file_number.times do |index|
+      File.write("./files/#{encrypt_key}/file_#{index}", file_data.slice!(0..line_maximum))
+    end
+  end
 
-master
+  def generate_digest_key(key)
+    digest = Digest::SHA256.new
+    digest.update(key)
+    digest.hexdigest
+  end
+end
